c073bf64182e440b7a73c5dc3990d5645524681c
Paula Alvarez C <alvarezpaula@outlook.com>
2023-11-24 14:53:22 -0300

    add SVG icons

--- lib/CustomUI/custom_card.dart
+++ lib/CustomUI/custom_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class CustomCard extends StatefulWidget {
   const CustomCard({super.key});
@@ -10,19 +11,33 @@ class CustomCard extends StatefulWidget {
 class _CustomCardState extends State<CustomCard> {
   @override
   Widget build(BuildContext context) {
-    return const ListTile(
-       leading: CircleAvatar(
-        radius: 25,
-       ),
-      title: Text("Paula", style: TextStyle( fontSize:16, fontWeight: FontWeight.bold),),
-      trailing: Text("18:04"), 
-      subtitle: Row(
+    return  InkWell(
+      onTap: () {},
+      child: Column(
         children: [
-          Icon(Icons.done_all, color: Colors.deepPurple,),
-          SizedBox( width: 3,),
-          Text('Hola, Como estas?', style: TextStyle( fontSize:13)),
-          ],
+          ListTile(
+             leading: CircleAvatar(
+              radius: 25,
+              child: SvgPicture.asset("assets/groups_black.svg", color: Colors.white70, height: 38, width: 38,),
+             ),
+            title: const Text("Paula", style: TextStyle( fontSize:16, fontWeight: FontWeight.bold),),
+            trailing: const Text("18:04"), 
+            subtitle: const Row(
+              children: [
+                Icon(Icons.done_all, color: Colors.deepPurple,),
+                SizedBox( width: 3,),
+                Text('Hola, Como estas?', style: TextStyle( fontSize:13)),
+                ],
+                ),
           ),
+          const Padding( 
+            padding:  EdgeInsets.only(right: 20, left: 80),
+            child:  Divider(
+            thickness: 1,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
