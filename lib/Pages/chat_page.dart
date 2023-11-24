fad665cf0576fa48a2eda4385c86a15317ad3c12
Paula Alvarez C <alvarezpaula@outlook.com>
2023-11-23 22:40:42 -0300

    add Font-family

--- lib/Pages/chat_page.dart
+++ lib/Pages/chat_page.dart
@@ -12,6 +12,12 @@ class _ChatPageState extends State<ChatPage> {
   Widget build(BuildContext context) {
     return  Scaffold(
       floatingActionButton: FloatingActionButton(
+        foregroundColor: Colors.blue,
+        splashColor: Colors.purple.shade100,
+        elevation: 12,  
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(40.0))
+        ),
         onPressed: () {
            print('');
         },   
